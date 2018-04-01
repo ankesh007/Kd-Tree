57934e7f742c264c8fffa126d552e3150b5c643e
ankesh007 <ankeshgupta007@gmail.com>
2018-04-01 06:04:40 +0530

    Added Comments in KD Creation to improve Readability

--- Src/KdTree.cpp
+++ Src/KdTree.cpp
@@ -5,13 +5,15 @@ kd_tree_node *make_tree(vector<vector<int> > DimSortedDataset,int dimCut)
 {
 	if(DimSortedDataset[0].size()==0)
 		return NULL;
+	//Empty Tree -> NULL
 
-	int instances=DimSortedDataset[0].size();
-	kd_tree_node *node=(kd_tree_node *)malloc(sizeof(kd_tree_node));
+	int instances=DimSortedDataset[0].size();	/* Points this Node Represents*/
+	kd_tree_node *node=(kd_tree_node *)malloc(sizeof(kd_tree_node)); 
 
 	node->minRect.resize(DIMENSIONS);
 	node->maxRect.resize(DIMENSIONS);
 
+	// Storing Minimum Dimension Value and Maximum Value helpful for KNN Query
 	for(int i=0;i<DIMENSIONS;i++)
 	{
 		node->minRect[i]=Dataset[DimSortedDataset[i][0]][i];
@@ -25,11 +27,13 @@ kd_tree_node *make_tree(vector<vector<int> > DimSortedDataset,int dimCut)
 	int medianDatapoint=-1;
 	double medianVal=-1;
 
+	// Deciding Median
 	medianDatapoint=DimSortedDataset[dimCut][instances/2];
 	medianVal=Dataset[DimSortedDataset[dimCut][instances/2]][dimCut];
 
-	node->Datapoint=Dataset[medianDatapoint];	
+	node->Datapoint=Dataset[medianDatapoint];	/*Setting Node Datapoint*/
 
+	// Divinding datapoints into 2 disjoint multiset
 	for(int i=0;i<DIMENSIONS;i++)
 	{
 		for(int j=0;j<instances;j++)
