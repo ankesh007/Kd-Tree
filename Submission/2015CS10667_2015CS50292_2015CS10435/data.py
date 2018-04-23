9c41ba9ac2338dfe823a9ed6863ff1657b5efdfa
ankesh007 <ankeshgupta007@gmail.com>
2018-04-23 18:07:09 +0530

    Finalizing and Pusshing Evaluation Meta data{

---
+++ Submission/2015CS10667_2015CS50292_2015CS10435/data.py
@@ -0,0 +1,24 @@
+import random
+
+n1=100000
+n2=100
+for d in [2,3,5,10,15,20]:
+
+	f = open('./Data/data' + str(d) + '.txt','w')
+	f.write(str(d)+" "+str(n1)+"\n")
+
+	for i in range(n1):
+		for j in range(d):
+			f.write(str(round(random.uniform(0,1),5))+" ")
+		f.write('\n')
+
+
+	q = open('./Data/query' + str(d) + '.txt', 'w')
+	q.write(str(d)+" "+str(n2)+"\n")
+	for i in range(n2):	
+		for j in range(d):
+			q.write(str(round(random.uniform(0, 1), 5)) + " ")
+		q.write('\n')
+	q.close()
+
+	print("d={} done".format(d))
---
+++ Submission/2015CS10667_2015CS50292_2015CS10435/mainseq.cpp
@@ -0,0 +1,152 @@
+#include "KdTree.h"
+
+vector<vd> Dataset;					  // Parsed from Input File
+vector<vector<int>> DimSortedDataset; // Precomputed Once -> Stores sorted Pointers to Original Dataset
+int DIMENSIONS;						  // To be set by Parser Function
+
+void parseInput(char *filename) //Parse and fill Dataset
+{
+	ifstream in;
+	in.open(filename);
+
+	int n; //number of points
+	in >> DIMENSIONS >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		vd temp;
+		for (int j = 0; j < DIMENSIONS; ++j)
+		{
+			double a;
+			in >> a;
+			temp.push_back(a);
+		}
+		Dataset.push_back(temp);
+	}
+}
+
+void preprocessing()
+{
+	int instances = Dataset.size();
+	DimSortedDataset.resize(DIMENSIONS);
+
+	cerr << instances << endl;
+	cerr << DIMENSIONS << endl;
+	for (int i = 0; i < DIMENSIONS; i++)
+	{
+		// cerr << i << endl;
+
+		DimSortedDataset[i].resize(instances);
+		for (int j = 0; j < instances; j++)
+		{
+			DimSortedDataset[i][j] = j;
+		}
+		sort(DimSortedDataset[i].begin(), DimSortedDataset[i].end(), CustomComparator(i));
+		// https://stackoverflow.com/questions/4066576/passing-a-parameter-to-a-comparison-function
+	}
+	// for(int i=0;i<DIMENSIONS;i++)
+	// {
+	// 	cout<<i<<"hi"<<endl;
+	// 	for(int j=0;j<instances;j++)
+	// 		cout<<DimSortedDataset[i][j]<<endl;
+	// }
+}
+
+int main(int argc, char *argv[])
+{
+	double d2=0,d100=0;
+
+	cerr << "started" << endl;
+	char *dataset_file = argv[1];
+	parseInput(dataset_file);
+	cerr << "input parsed" << endl;
+	preprocessing();
+
+
+	// Request name/path of query_file from parent by just sending "0" on stdout
+	ofstream rfile;
+
+	// rfile.open("results.txt");
+	cout << 0 << endl;
+
+	// Wait till the parent responds with name/path of query_file and k | Timer will start now
+	char *query_file = new char[100];
+	cin >> query_file;
+
+	ifstream qfile;
+	qfile.open(query_file);
+
+	int d, n2;
+	qfile >> d >> n2;
+	int k;
+	cin >> k;
+
+	string resfile = "./Results/resultsseq_" + to_string(k) + "_" + to_string(d)+".txt" ;
+	rfile.open(resfile);
+
+	cerr << "k received" << endl;
+
+	cerr << n2 << " queries" << endl;
+	for (int z = 0; z < n2; ++z)
+	{
+		// cerr <<"z="<< z << endl;
+		vector<double> query_point;
+		for (int i = 0; i < d; ++i)
+		{
+			double a;
+			qfile >> a;
+			query_point.push_back(a);
+		}
+
+		naive_knn(query_point, k);
+
+		vector<pair<double, int>> result;
+		int len = naive_max_heap.size();
+		result.resize(len);
+		int index = len;
+
+		while (!naive_max_heap.empty())
+		{
+			index--;
+			pair<double, int> r = naive_max_heap.top();
+			naive_max_heap.pop();
+			result[index] = r;
+		}
+
+		// for (int i = 0; i < d; ++i)
+		// {
+		// 	rfile << query_point[i] << " ";
+		// }
+		// rfile << "\n";
+
+		if(k==100)
+		{
+			d2+= result[1].first;
+			d100 += result[99].first;
+		} 
+		for (int i = 0; i < len; ++i)
+		{
+			int idx = result[i].second ;
+			// cerr<<"idx-"<<idx<<endl;
+			for (int j = 0; j < DIMENSIONS; ++j)
+			{
+				rfile << (Dataset[idx][j]) << " ";
+			}
+			// rfile << result[i].first;
+			rfile	  << "\n";
+		}
+	}
+
+	if(k==100)
+	{
+		cerr<<"Avg dist to 2nd closest=  "<<d2/n2<<endl;
+		cerr << "Avg dist to 100th closest=" << d100 / n2 << endl;
+		cerr<<d2/d100<<endl;
+	}
+	cout << 1 << endl;
+
+	rfile.close();
+
+	// [TODO] Read the query point from query_file, do kNN using the kdTree and output the answer to results.txt
+
+	// Convey to parent that results.txt is ready by sending "1" on stdout | Timer will stop now and this process will be killed
+}
